8ed8ee3d7f0af7da19412fccda8cc6174e3f525d
earlbalageo-dev <earlbalageo.dev@gmail.com>
2021-06-12 05:22:16 -0700

    header bug

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import { Container, Navbar, Nav } from 'react-bootstrap';
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
+  const logoutHandler = () => {
+    console.log('logout');
+  };
+
   return (
     <header>
       <Navbar bg='light' expand='lg'>
@@ -14,14 +22,26 @@ const Header = () => {
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Nav className='ml-auto'>
-              <LinkContainer to='/login'>
-                <Nav.Link>Login</Nav.Link>
-              </LinkContainer>
-              <LinkContainer to='/register'>
-                <Nav.Link>Register</Nav.Link>
-              </LinkContainer>
-
               <Nav.Link href='#link'>Cart</Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.firstName} id='userName'>
+                  <LinkContainer to='/profile'>
+                    <NavDropdown.Item>Profile</NavDropdown.Item>
+                  </LinkContainer>
+                  <NavDropdown.Item onClick={logoutHandler}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <>
+                  <LinkContainer to='/login'>
+                    <Nav.Link>Login</Nav.Link>
+                  </LinkContainer>
+                  <LinkContainer to='/register'>
+                    <Nav.Link>Register</Nav.Link>
+                  </LinkContainer>{' '}
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
