6deadbe9bc00ca5d8fc77144738334460dce0c5e
earlbalageo-dev <earlbalageo.dev@gmail.com>
2021-07-02 01:18:26 -0700

    product carousel

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Container, Image, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './Loader';
 import Message from './Message';
@@ -21,17 +21,27 @@ const ProductCarousel = () => {
   ) : error ? (
     <Message variant='danger'>{error}</Message>
   ) : (
-    <Carousel pause='hover' className='bg-dark'>
+    <Carousel className='carou' pause='hover' className='bg-dark'>
       {products.map((product) => (
         <Carousel.Item key={product._id}>
-          <Link to={`/product/${product._id}`}>
+          <img
+            className='d-block w-100'
+            src={product.image}
+            alt={product.name}
+          />
+          <Carousel.Caption className='carousel-caption'>
+            <h3>
+              {product.name} (${product.price})
+            </h3>
+          </Carousel.Caption>
+          {/* <Link to={`/product/${product._id}`}>
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2>
                 {product.name} (${product.price})
               </h2>
             </Carousel.Caption>
-          </Link>
+          </Link> */}
         </Carousel.Item>
       ))}
     </Carousel>
